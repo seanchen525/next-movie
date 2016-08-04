51012f4ef76dccca9c5a7b201572df51f50a4e82
warrior <warriorbrant@gmail.com>
2016-08-03 20:41:13 -0400

    cascading function

--- data/playlist.js
+++ data/playlist.js
@@ -1,8 +1,10 @@
 mongoCollections = require("../config/mongoCollections");
-Playlist = mongoCollections.playlist;
+Playlist = mongoCollections.Playlist;
+var movies=require('./Movie');
 var uuid = require('node-uuid');
 
 var exportedMethods = {
+    //operations related to playlist
     getAllPlaylist() {
         return Playlist().then((playlistCollection) => {
             return playlistCollection.find({}).toArray();
@@ -12,43 +14,31 @@ var exportedMethods = {
     // methods on an object with this shorthand!
     getPlaylistById(id) {
         return Playlist().then((playlistCollection) => {
-            return playlistCollection.findOne({ _id: id }).then((playlistObj) => {
+            return playlistCollection.findOne({_id:id}).then((playlistObj) => {
                 if (!playlistObj) throw "Playlist not found";
                 return playlistObj;
-            }).catch((error) => {
-                return error;
-            });
+            }).catch((error)=>{
+				return error;
+			});
         });
     },
-
-    setNewTitle(playlistId, newTitle) {
+	
+	addPlaylistGeneral(obj) {
         return Playlist().then((playlistCollection) => {
-            return playlistCollection.updateOne({ _id: playlistId }, { $set: { "title": newTitle } }).then(function () {
-                return playlistId;
-            }).then(id => {
-                return this.getPlaylistById(id);
-            });
-        }).catch((error) => {
-            return { error: error };
+            obj["_id"]=uuid.v4();
+            return playlistCollection.insertOne(obj).then((playlistObj) => {
+               return playlistObj.insertedId;
+            }).then(newId=>{
+				return this.getPlaylistById(newId);
+			});
         });
-
     },
-    getPlaylistByUserId(userId) {
-        return Playlist().then((playlistCollection) => {
-            return playlistCollection.findOne({ "user._id": userId }).then((playlist) => {
-                if (!playlist) throw "User has no playlist";
-                return playlist;
-            }).catch((error) => {
-                return error;
-            });
-        });
 
-    },
 
     addPlaylist(title, user) {
         return Playlist().then((playlistCollection) => {
-            let playlistId = uuid.v4();
-            let obj = {
+            var playlistId = uuid.v4();
+            var obj = {
                 _id: playlistId,
                 title: title,
                 user: user,
@@ -62,16 +52,30 @@ var exportedMethods = {
         });
     },
 
-    deletePlaylistById(id) {
+
+    getPlaylistByUserId(userId) {
         return Playlist().then((playlistCollection) => {
-            return playlistCollection.deleteOne({ _id: id }).then(function (deletionInfo) {
-                if (deletionInfo.deletedCount === 0) throw "Could not find the document with this id to delete";
-                return true;
+            return playlistCollection.findOne({ "user._id": userId }, { _id: 1 }).then((playlist) => {
+                if (!playlist) throw "User has no playlist";
+                return playlist;
+            }).catch((error) => {
+                return error;
             });
-        }).catch((error) => {
-            return error;
-        })
+        });
+
     },
+	
+	deletePlaylistById(id){
+		return Playlist().then((playlistCollection) => {
+            return playlistCollection.deleteOne({ _id: id }).then(function(deletionInfo) {
+            	if(deletionInfo.deletedCount === 0) throw "Could not find the document with this id to delete";
+				return true;
+        	});
+        }).catch((error)=>{
+			return error;
+		})
+	},
+
 
     clearPlaylist(id) {
         return Playlist().then((playlistCollection) => {
@@ -84,35 +88,25 @@ var exportedMethods = {
             return { error: error };
         });
     },
+	  
+    updatePlaylistById(id,obj){
+		return Playlist().then((playlistCollection) => {
+            return playlistCollection.update({ _id: id },{$set:obj}).then(function() {
+				//console.log(typeof this.getRecipeById(id));
+            	return id;
+        	});
+        }).then(id=>{
+			return this.getPlaylistById(id);
+		}).catch((error)=>{
+			return error;
+		})
+	},
 
-    checkOffMovie(playlistId, movieId) {
-        return Playlist().then((playlistCollection) => {
-            return playlistCollection.update({ _id: playlistId, playlistMovies: { $elemMatch: { _id: movieId } } }, { $set: { "playlistMovies.$.viewed": true } }).then(function () {
-                return id;
-            });
-        }).then(id => {
-            return this.getPlaylistById(id);
-        }).catch((error) => {
-            return error;
-        })
-    },
 
-    updatePlaylistById(id, obj) {
-        return Playlist().then((playlistCollection) => {
-            return playlistCollection.update({ _id: id }, { $set: obj }).then(function () {
-                //console.log(typeof this.getRecipeById(id));
-                return id;
-            });
-        }).then(id => {
-            return this.getPlaylistById(id);
-        }).catch((error) => {
-            return error;
-        })
-    },
-
+    //operations related to movies
     addMovieToPlaylist(playlistId, movieId, title, overview) {   //add movie to the playlistMovies array by providing playlist id and the movie object.Node:the review id in the movie should be added first
         return Playlist().then((playlistCollection) => {
-            let obj = {
+            var obj = {
                 _id: movieId,
                 title: title,
                 overview: overview,
@@ -128,58 +122,124 @@ var exportedMethods = {
         });
     },
 
-    getMovieByMovieId(pid, mid) {     //get the movie from the playlist by providing the specified playlist id and the movie id
+    //Cascading method:add movie to the playlistMovies array and the movie collection by providing playlist id and the movie object.
+    //Note:the movieObj should  be same as the movie collection and when you invoke this cascade method, you need to create this obj
+    //in the client side and make sure that this movie doesn't exist in the movie collection
+    //the movieObj should contain: _id,title,description,genre,rated,releaseDate,runtime,director,cast,averageRating,keywords,and empty allreviews
+    //this method will return movieObj inserted into the movie collection
+    addMovieToPlaylistAndMovie(playlistId,movieObj) {   
         return Playlist().then((playlistCollection) => {
-            return playlistCollection.findOne({ _id: pid }).then((playlistObj) => {
-                if (!playlistObj) throw "Playlist with id " + pid + " doesn't exist!";
-                var movielist = playlistObj.playlistMovies;
-                console.log(movielist);
-                for (var i = 0; i < movielist.length; i++) {
-                    if (movielist[i]._id == mid) return movielist[i];
-                }
-                return "not find";
+            var obj = {
+                _id: movieObj._id,
+                title: movieObj.title,
+                overview: movieObj.description,
+                viewed: false
+            };
+            return playlistCollection.update({ _id: playlistId }, { $addToSet: { "playlistMovies": obj } }).then(function () {
+                return movieObj;
+            }).then((movieObj) => {
+                return movies.addMovieGeneral(movieObj).then((Movie)=>{
+                    return Movie;
+                });
             }).catch((error) => {
                 return { error: error };
             });
-        }).catch((error) => {
-            return { error: error };
+        });
+    },
+
+
+    addMovieToPlaylistGeneral(id,obj) {   //add movie to the playlistMovies array by providing playlist id and the movie object.Node:the review id in the movie should be added first
+        return Playlist().then((playlistCollection) => {
+            //obj["review"]["_id"]=uuid.v4();
+            return playlistCollection.update({ _id: id },{$addToSet:{ "playlistMovies": obj } }).then(function() {
+                return id;
+            }).then(id=>{
+                return this.getPlaylistById(id);
+            }).catch((error)=>{
+                return {error:error};
+            });
+        });
+    },
+
+    getMovieByMovieId(pid,mid){     //get the movie from the playlist by providing the specified playlist id and the movie id
+        return Playlist().then((playlistCollection)=>{
+            return playlistCollection.findOne({_id:pid}).then((playlistObj)=>{
+                if(!playlistObj) throw "Playlist with id "+pid+" doesn't exist!";
+                var movielist=playlistObj.playlistMovies;
+                console.log(movielist);
+                for(var i=0;i<movielist.length;i++){
+                    if(movielist[i]._id==mid) return movielist[i];
+                }
+                return "not find";
+            }).catch((error)=>{
+                return {error:error};
+            });
+        }).catch((error)=>{
+                return {error:error};
         });
 
     },
 
-    removeMovieByMovieId(pId, mId) {   //delete specified movie in the playlistMovies array by providing playlist id and the movie id
-        return Playlist().then((playlistCollection) => {
-            return playlistCollection.update({ _id: pId }, { $pull: { "playlistMovies": { _id: mId } } }).then(function () {
+    removeMovieByMovieId(pId,mId){   //delete specified movie in the playlistMovies array by providing playlist id and the movie id
+        return Playlist().then((playlistCollection)=>{
+            return playlistCollection.update({ _id: pId }, { $pull: { "playlistMovies": { _id: mId } } }).then(function() {
                 return pId;
+            }).then(id=>{
+                return this.getPlaylistById(id);
+            });
+        }).catch((error)=>{
+                return {error:error};
+        });
+    },
+
+    updateMovieByMovieId(pid,mid,obj){   //delete specified movie in the playlistMovies array by providing playlist id and the movie id
+        return Playlist().then((playlistCollection)=>{
+            return playlistCollection.update({ _id: pid,playlistMovies:{$elemMatch: {_id:mid }}}, { $set: { "playlistMovies.$": obj } }).then(function() {
+                return pid;
+            }).then((pid)=>{
+                return this.getMovieByMovieId(pid,mid).then((movieObj)=>{
+                    return movieObj;
+                });
+            }).catch((error)=>{
+                return {error:error};
+            });
+        }).catch((error)=>{
+                return {error:error};
+        });
+    },
+    
+
+    //other operations
+    setNewTitle(playlistId, newTitle) {
+        return Playlist().then((playlistCollection) => {
+            return playlistCollection.updateOne({ _id: playlistId }, { $set: { "title": newTitle } }).then(function () {
+                return playlistId;
             }).then(id => {
                 return this.getPlaylistById(id);
             });
         }).catch((error) => {
             return { error: error };
         });
+
     },
 
-    updateMovieByMovieId(pid, mid, obj) {   //delete specified movie in the playlistMovies array by providing playlist id and the movie id
+    checkOffMovie(playlistId, movieId) {
         return Playlist().then((playlistCollection) => {
-            return playlistCollection.update({ _id: pid, playlistMovies: { $elemMatch: { _id: mid } } }, { $set: { "playlistMovies.$": obj } }).then(function () {
-                return pid;
-            }).then((pid) => {
-                return this.getMovieByMovieId(pid, mid).then((movieObj) => {
-                    return movieObj;
-                });
-            }).catch((error) => {
-                return { error: error };
+            return playlistCollection.update({ _id: playlistId, playlistMovies: { $elemMatch: { _id: movieId } } }, { $set: { "playlistMovies.$.viewed": true } }).then(function () {
+                return id;
             });
+        }).then(id => {
+            return this.getPlaylistById(id);
         }).catch((error) => {
-            return { error: error };
-        });
+            return error;
+        })
     },
 
 
     addMovieReviewToPlaylist(playlistId, movieId, review) {
         return Playlist().then((playlistCollection) => {
-            let reviewId = uuid.v4();
-            let obj = {
+            var reviewId = uuid.v4();
+            var obj = {
                 _id: reviewId,
                 rating: review.rating,
                 date: review.date,
@@ -197,11 +257,40 @@ var exportedMethods = {
 
     },
 
+    //this is a cascading method which can add the review to both the movie in the playlist and the movie in the movie collection
+    //In order to invoke this method, you need to provide the playlist id, movie id and the completed view with out review id(the review id will be created by this function)
+    //the review should contain: rating,date,poster and comment and these attribues should be create by client who invokes this method 
+    addMovieReviewToPlaylistAndMovie(playlistId, movieId, review) {
+        return Playlist().then((playlistCollection) => {
+            var reviewId = uuid.v4();
+            var obj = {
+                _id: reviewId,
+                rating: review.rating,
+                date: review.date,
+                comment: review.comment
+            };
+            return playlistCollection.update({ _id: playlistId, "playlistMovies._id": movieId }, { $set: { "playlistMovies.$.review": obj } }).then(function () {
+                return obj;
+            }).then((obj) => {
+                obj["poster"]=review.poster;
+                return movies.addReviewToMovieGeneral(movieId,obj).then((Movie)=>{
+                    return Movie;
+                });
+            }).catch((error) => {
+                console.log("error");
+                return { error: error };
+            });
+        });
+
+    },
+
+
+
     removeReviewFromPlaylist(playlistId, reviewId) {
         return Playlist().then((playlistCollection) => {
-            return playlistCollection.updateOne({ _id: playlistId, "playlistMovies.review._id": reviewId }, {
-                //unset review object
-                $unset: { "playlistMovies.$.review": "" }
+            return playlistCollection.updateOne({ _id: playlistId }, {
+                //pull comment that matches the given id
+                $unset: { "playlistMovies.0.review": "" }
             }).then((result) => {
                 if (result.modifiedCount == 0) throw "Could not remove review with id of " + reviewId;
             }).catch((error) => {
@@ -226,6 +315,7 @@ var exportedMethods = {
             });
         });
     }
+
 
 }
 
--- data/users.js
+++ data/users.js
@@ -1,5 +1,6 @@
 mongoCollections = require("../config/mongoCollections");
-Users = mongoCollections.users;
+Users = mongoCollections.Users;
+var playlist=require('./Playlist');
 var uuid = require('node-uuid');
 
 var exportedMethods = {
@@ -12,59 +13,123 @@ var exportedMethods = {
     // methods on an object with this shorthand!
     getUserById(id) {
         return Users().then((userCollection) => {
-            return userCollection.findOne({ _id: id }).then((userObj) => {
+            return userCollection.findOne({_id:id}).then((userObj) => {
                 if (!userObj) throw "Users not found";
                 return userObj;
-            }).catch((error) => {
-                return error;
-            });
+            }).catch((error)=>{
+				return error;
+			});
+        });
+    },
+    
+    getUserBySessionId(id) {
+        return Users().then((userCollection) => {
+            return userCollection.findOne({sessionId:id}).then((userObj) => {
+                if (!userObj) throw "Users not found";
+                return userObj;
+            }).catch((error)=>{
+				return error;
+			});
+        });
+    },
+	
+	addUsersGeneral(obj) {
+        return Users().then((userCollection) => {
+        	obj["_id"]=uuid.v4();
+            obj["profile"]["_id"]= obj["_id"];
+            return userCollection.insertOne(obj).then((userObj) => {
+               return userObj.insertedId;
+            }).then(newId=>{
+				return this.getUserById(newId);
+			});
         });
     },
 
-    addUser(username, name, email) {
+    addUsers(sessionId,hashedPassword,profile,preferences) {
+        var userid=uuid.v4();
+        profile["_id"]=userid;
+        var obj={
+            _id:userid,
+            sessionId:sessionId,
+            hashedPassword:hashedPassword,
+            profile:profile,
+            preferences:preferences
+        };
         return Users().then((userCollection) => {
-            console.log("Adding user");
-            let userId = uuid.v4();
-            let obj = {
-                _id: userId,
-                profile: {
-                    _id: userId,
-                    username: username,
-                    name: name,
-                    email: email
-                }
-            };
-            console.log(obj);
             return userCollection.insertOne(obj).then((userObj) => {
-                return userObj.insertedId;
-            }).then(newId => {
+               return userObj.insertedId;
+            }).then(newId=>{
                 return this.getUserById(newId);
             });
         });
     },
 
-    deleteUserById(id) {
-        return Users().then((userCollection) => {
-            return userCollection.deleteOne({ _id: id }).then(function (deletionInfo) {
-                if (deletionInfo.deletedCount === 0) throw "Could not find the document with this id to delete";
-                return true;
-            });
-        }).catch((error) => {
-            return error;
-        })
-    },
 
-    updateUserById(id, obj) {
+    //This is a cascading method which can create user and the user's playlist
+    //please note that: the title is the title for playlist and the userObj contains:sessionId,hashedPassword,profile,preferences
+    //the profile attribute doesn't contain the _id and the _id will be created by this function
+    addUsersAndPlaylist(title,userObj) {
+        var userid=uuid.v4();
+        userObj.profile["_id"]=userid;
+        var obj={
+            _id:userid,
+            sessionId:userObj.sessionId,
+            hashedPassword:userObj.hashedPassword,
+            profile:userObj.profile,
+            preferences:userObj.preferences
+        };
         return Users().then((userCollection) => {
-            return userCollection.update({ _id: id }, { $set: obj }).then(function () {
-                //console.log(typeof this.getRecipeById(id));
-                return id;
+            return userCollection.insertOne(obj).then((userObj) => {
+               return obj;
+            }).then(obj=>{
+                var user={
+                    _id:obj._id,
+                    username:obj.profile.username,
+                    name:obj.profile.name,
+                    email:obj.profile.name
+                }
+                return playlist.addPlaylist(title,user).then((playlistObj)=>{
+                    return playlistObj;
+                }) ;
             });
-        }).then(id => {
-            return this.getUserById(id);
-        }).catch((error) => {
-            return error;
-        })
+        });
+    },
+	
+	deleteUserById(id){
+		return Users().then((userCollection) => {
+            return userCollection.deleteOne({ _id: id }).then(function(deletionInfo) {
+            	if(deletionInfo.deletedCount === 0) throw "Could not find the document with this id to delete";
+				return true;
+        	});
+        }).catch((error)=>{
+			return error;
+		})
+	},
+	  
+    updateUserById(id,obj){
+		return Users().then((userCollection) => {
+            return userCollection.update({ _id: id },{$set:obj}).then(function() {
+				//console.log(typeof this.getRecipeById(id));
+            	return id;
+        	});
+        }).then(id=>{
+			return this.getUserById(id);
+		}).catch((error)=>{
+			return error;
+		})
+	},
+    
+    verifyUser(obj){
+        return Users().then((userCollection) => {
+            return userCollection.findOne({$and: [{"profile.username": obj.username}, {hashedPassword: obj.password}]}).then((userObj) => {
+                if (!userObj) throw "Users not found";
+                
+                userObj.sessionId = uuid.v4();
+                return this.updateUserById(userObj._id, userObj);;
+            }).catch((error)=>{
+				return error;
+			});
+        });
     }
 
 }
