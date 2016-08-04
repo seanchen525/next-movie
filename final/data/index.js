0d2d4f7936fe26b2c3305efa49f4a0d50702869a
warrior <warriorbrant@gmail.com>
2016-08-03 21:04:07 -0400

    delete

--- final/data/index.js
+++
@@ -1,9 +0,0 @@
-const movie = require("./Movie");
-const playlist = require("./Playlist");
-const users = require("./Users");
-
-module.exports = {
-    movie: movie,
-    playlist: playlist,
-    users: users
-};
