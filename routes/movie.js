5218b17391761d2afc0e5d4504ae24396f1faf28
warrior <warriorbrant@gmail.com>
2016-08-03 21:00:07 -0400

    test code for db

---
+++ routes/movie.js
@@ -0,0 +1,111 @@
+/**
+ * @author warri
+ */
+ var express = require('express');
+ var movies=require('../data/Movie')
+ var uuid = require('node-uuid');
+ var router = express.Router();
+ 
+ router.get('/movies', function (req, res) {
+  	 var list=movies.getAllMovie().then((Movielist)=>{
+		if (Movielist) {
+			res.status(200).send(Movielist);
+		}else{
+			res.sendStatus(404);
+		}
+	 });
+  }),
+
+  router.get('/movies/:id', function (req, res) {
+  	 movies.getMovieById(req.params.id).then((MovieObj)=>{
+		if (MovieObj) {
+			res.render("movie/detail", {
+				movie: MovieObj,
+				partial: "jquery-detail-scripts"
+			});
+		}else{
+			res.sendStatus(404);
+		}
+	 });
+  }),
+  
+  router.post('/movies', function (req, res) {
+  	 var obj=req.body;
+  	 movies.addMovie(obj).then((MovieObj)=>{
+		if (MovieObj) {
+			res.status(200).send(MovieObj);
+		}else{
+			res.sendStatus(404);
+		}
+	 });
+  }),
+  
+  router.put('/movies/:id', function (req, res) {
+  	 movies.updateMovieById(req.params.id,req.body).then((MovieObj)=>{
+		if (MovieObj) {
+			//console.log(MovieObj);
+			res.status(200).send(MovieObj);
+		}else{
+			res.sendStatus(404);
+		}
+	 });
+  }),
+  
+  router.delete('/movies/:id', function (req, res) {
+  	  movies.deleteMovieById(req.params.id).then((MovieObj)=>{
+		if (MovieObj) {
+			res.status(200).send(MovieObj);
+		}else{
+			res.sendStatus(404);
+		}
+	 });
+  }),
+
+    router.post('/movies/review/:id', function (req, res) {
+  	 movies.addReviewToMovie(req.params.id,req.body).then((movieObj)=>{
+		if (movieObj) {
+			//console.log(PlaylistObj);
+			res.status(200).send(movieObj);
+		}else{
+			res.sendStatus(404);
+		}
+	 });
+  }),
+
+  router.get('/movies/review/:mid/:rid', function (req, res) {
+  	 movies.getReviewByReviewId(req.params.mid,req.params.rid).then((reviewObj)=>{
+		if (reviewObj) {
+			//console.log(PlaylistObj);
+			res.status(200).send(reviewObj);
+		}else{
+			res.sendStatus(404);
+		}
+	 });
+  }),
+
+  router.delete('/movies/review/:mid/:rid', function (req, res) {
+  	 movies.removeReviewByReviewId(req.params.mid,req.params.rid).then((movieObj)=>{
+		if (movieObj) {
+			//console.log(PlaylistObj);
+			res.status(200).send(movieObj);
+		}else{
+			res.sendStatus(404);
+		}
+	 });
+  }),
+
+  router.put('/movies/review/:mid/:rid', function (req, res) {
+  	 movies.updateReviewByReviewId(req.params.mid,req.params.rid,req.body).then((reviewObj)=>{
+		if (reviewObj) {
+			//console.log(PlaylistObj);
+			res.status(200).send(reviewObj);
+		}else{
+			res.sendStatus(404);
+		}
+	 });
+  }),
+
+
+ 
+  module.exports = router;
+ 
---
+++ routes/user.js
@@ -0,0 +1,106 @@
+/**
+ * @author warri
+ */
+ var express = require('express');
+ var users=require('../data/Users')
+ var router = express.Router();
+ const xss = require('xss');
+ 
+ router.get('/users', function (req, res) {
+  	 var list=users.getAllUser().then((userlist)=>{
+		if (userlist) {
+			res.status(200).send(userlist);
+		}else{
+			res.sendStatus(404);
+		}
+	 });
+  }),
+  
+  router.get('/login', function (req, res) {
+  	  res.render("layouts/login", {
+		  partial: "jquery-login-scripts"
+	  });
+  }),
+
+  router.get('/user', function (req, res) {
+	 if (req.cookies.next_movie == undefined) res.redirect("/login");
+  	 users.getUserBySessionId(req.cookies.next_movie).then((userObj)=>{
+		if (userObj) {
+			res.render("user/index", {
+				user: userObj,
+				partial: "jquery-user-index-scripts"
+			});
+		}else{
+			res.sendStatus(404);
+		}
+	 });
+	 
+  }),
+  
+  router.post('/users', function (req, res) {
+  	 var obj=req.body;
+<<<<<<< HEAD
+  	 users.addUsersGeneral(obj).then((userObj)=>{
+		if (userObj) {
+			res.status(200).send(userObj);
+		}else{
+			res.sendStatus(404);
+		}
+	 });
+  }),
+
+  router.post('/users/playlist/:title', function (req, res) {
+  	 var obj=req.body;
+  	 users.addUsersAndPlaylist(req.params.title,obj).then((userObj)=>{
+=======
+	 //obj["_id"]=uuid.v4();
+	 //obj["profile"]["_id"]= obj["_id"];
+  	 users.addUsers(obj).then((userObj)=>{
+>>>>>>> pb/master
+		if (userObj) {
+			res.status(200).send(userObj);
+		}else{
+			res.sendStatus(404);
+		}
+	 });
+  }),
+  
+  router.put('/users/:id', function (req, res) {
+  	 users.updateUserById(req.params.id,req.body).then((userObj)=>{
+		if (userObj) {
+			//console.log(userObj);
+			res.status(200).send(userObj);
+		}else{
+			res.sendStatus(404);
+		}
+	 });
+  }),
+  
+  router.delete('/users/:id', function (req, res) {
+  	  users.deleteUserById(req.params.id).then((userObj)=>{
+		if (userObj) {
+			res.status(200).send(userObj);
+		}else{
+			res.sendStatus(404);
+		}
+	 });
+  });
+  
+router.post('/user/login', function (req, res) {
+	var userObj = {};
+	userObj.username = req.body.username;
+	userObj.password = req.body.password;
+	//When to fire the session?
+	users.verifyUser(userObj).then((user) => {
+		if (user != "Users not found"){
+			res.cookie("next_movie", user.sessionId, { expires: new Date(Date.now() + 24 * 3600000)}); 
+			res.json({ success: true });
+			return;
+		} else {
+			res.json({ success: false, message: "username or password is invalid" });
+		}
+	});
+}),
+ 
+  module.exports = router;
+ 
